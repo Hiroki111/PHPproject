b041de1e73a2db6eadfb376c308eadc0b1a85b91
Hiroki <mitsuke1116@gmail.com>
2015-08-22 08:05:22 +1000

    a

---
+++ HeaderTest.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head><title>Pattern mining project/Header and Footer test</title></head>
+
+<body>
+<?php 
+	
+
+	echo "<p>Header/Footer</p>\n\n";
+	
+	
+	echo "(Test done)";
+?>
+
+</body>
+
+</html>
