8b7b24f2a5f27aeae3a511f25029063456af43e4
Hiroki <mitsuke1116@gmail.com>
2015-10-26 10:45:11 +1000

    Replacing invalid single quotation marks
    Invalid single quotationa marks will be replaced with valid ones

--- pattern.php
+++ pattern.php
@@ -25,6 +25,7 @@
 					or die("Coudn't execute query. " . mysqli_error($cxn));
 					
 				?>
+				
 				<script type ="text/javascript">
 					$(document).ready(function() 
 					{
@@ -43,19 +44,30 @@
 				</script>
 				<?php
 					while ($data = mysqli_fetch_array($result)) {
+						$remove = array("‘", "’"); //Both "‘", "’" are often garbled, so they need to be replaced
 						echo '<h3> Pattern Name: </h3><p>' . $data['PatternName'] . "\n</p>";
-						echo "<h3> Description: </h3><p>" . $data['Description'] . "</p>\n";
-						echo '<h3> Real Life Example: </h3><p>' . $data['RealLifeExample'] . "</p>\n";
-						echo '<h3> Affect: </h3><p>' . $data['Affect'] . "</p>\n";
-						echo '<h3> Manifestation: </h3><p>' . $data['Manifestation'] . "</p>\n";
-						echo '<h3> Remedy: </h3><p>' . $data['Remedy'] . "</p>\n";
-						echo '<h3> SIde Effect: </h3><p>' . $data['SideEffect'] . "</p>\n";
-						echo '<h3> Quality Issue Name: </h3><p>' . $data['QualityIssueName'] . "</p>\n";
-						echo '<h3> Quality Issue: </h3><p>' . $data['QualityIssue'] . "</p>\n";
+						$description = str_replace($remove,"'", $data['Description']);
+						echo "<h3> Description: </h3><p>" . $description . "</p>\n";
+						$realLifeExample = str_replace($remove,"'", $data['RealLifeExample']);
+						echo '<h3> Real Life Example: </h3><p>' . $realLifeExample . "</p>\n";
+						$affect = str_replace($remove,"'", $data['Affect']);
+						echo '<h3> Affect: </h3><p>' . $affect . "</p>\n";
+						$manifestation = str_replace($remove,"'", $data['Manifestation']);
+						echo '<h3> Manifestation: </h3><p>' . $manifestation . "</p>\n";
+						$remedy = str_replace($remove,"'", $data['Remedy']);
+						echo '<h3> Remedy: </h3><p>' . $remedy . "</p>\n";
+						$sideEffect = str_replace($remove,"'", $data['SideEffect']);
+						echo '<h3> Side Effect: </h3><p>' . $sideEffect . "</p>\n";
+						$qualityIssueName = str_replace($remove,"'", $data['QualityIssueName']);
+						echo '<h3> Quality Issue Name: </h3><p>' . $qualityIssueName . "</p>\n";
+						$qualityIssue = str_replace($remove,"'", $data['QualityIssue']);
+						echo '<h3> Quality Issue: </h3><p>' . $qualityIssue . "</p>\n";
 					}
-				?>   
-			</div> <!-- end content -->   
-			<?php include("footer.html"); ?><!-- end footer -->          
+				?>
+				
+				
+					</div> <!-- end content -->   
+					<?php include("footer.html"); ?><!-- end footer -->          
 		</div> <!-- end mainContainer --> 
 	</body> 
 </html>
